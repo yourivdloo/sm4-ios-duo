a8a2d156307c16fe0546b72e1b389615bbef2c7e
youri <youri.yvdl@gmail.com>
2021-03-05 15:30:57 +0100

    Added basic QuestionView layout and included buttons in body selector

--- General practitioner/General practitioner/BodyView.swift
+++ General practitioner/General practitioner/BodyView.swift
@@ -10,10 +10,88 @@ import SwiftUI
 struct BodyView : View {
     var body: some View {
         NavigationView{
-            
-            Image("beter").resizable()
-                .scaledToFit()
-                .navigationBarTitle("General practitioner")
+            ZStack{
+            VStack{
+                //            Text("Where do you ache?")
+                //                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                Image("body")
+                    .resizable()
+                    .padding([.horizontal], -80)
+            }
+                    Button("Hand"){}
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color.blue)
+                        .opacity(0.5)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                        .offset(x: 150, y: 0.0)
+                        
+                    Button("Arm"){}
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color.blue)
+                        .opacity(0.5)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                        .offset(x: 78, y: -72.0)
+                    
+                    Button("Shoulder"){}
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color.blue)
+                        .opacity(0.5)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                        .offset(x: 30, y: -160.0)
+                
+                Button("Head"){}
+                    .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.blue)
+                    .opacity(0.5)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .offset(x: -7.0, y: -270.0)
+                
+                Button("Knee"){}
+                    .frame(width: 100, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.blue)
+                    .opacity(0.5)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .offset(x: -93.0, y: 155)
+                
+                Button("Hip"){}
+                    .frame(width: 100, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.blue)
+                    .opacity(0.5)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .offset(x: -50.0, y: 40)
+                
+                Button("Belly"){}
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .background(Color.blue)
+                    .opacity(0.5)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .offset(x: -7.0, y: -50)
+                
+                Button("Leg"){}
+                    .frame(width: 100, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.blue)
+                    .opacity(0.5)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .offset(x: 30.0, y: 90)
+                
+                Button("Foot"){}
+                    .frame(width: 100, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.blue)
+                    .opacity(0.5)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .offset(x: 50.0, y: 280)
+        }
+            .navigationBarTitle("Where do you ache?")
         }
     }
 }
--- General practitioner/General practitioner/QuestionView.swift
+++ General practitioner/General practitioner/QuestionView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct QuestionView: View {
     var body: some View {
+        VStack{
         Text("Question")
+            .font(.title)
+            HStack{
+                Button("YES") {
+                }
+                .font(.title2)
+                .frame(width: 150, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.green)
+                .padding()
+                .overlay(
+                  RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.green, lineWidth: 1)
+                )
+                Button("NO") {
+                }
+                .font(.title2)
+                .frame(width: 150, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.red)
+                .padding()
+                .overlay(
+                  RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.red, lineWidth: 1)
+                )
+            }
+            Button("Not sure") {
+            }
+            .font(.title2)
+            .foregroundColor(.blue)
+        }
     }
 }
 
